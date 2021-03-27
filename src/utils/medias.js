21b3a846130a771bd1ce8c426394a7a9a835ce32
Muhamad Iqbal Tawakal <tawakalisme@gmail.com>
2021-03-28 05:54:45 +0700

    renaming url

--- src/utils/medias.js
+++ src/utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith("/")) {
     // Prepend Strapi address
-    return `${process.env.GRIDSOME_STRAPI_URL ||
+    return `${"https://qblt-strapi.herokuapp.com" ||
       "http://localhost:1337"}${url}`;
   }
   // Otherwise return full URL
