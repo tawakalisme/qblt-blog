f298fc59f6c2e3cc9577ea64a1d66eb14a3fb1b1
Muhamad Iqbal Tawakal <tawakalisme@gmail.com>
2021-04-08 22:06:41 +0700

    Final Version

--- src/templates/Portfolio.vue
+++ src/templates/Portfolio.vue
@@ -2,22 +2,15 @@
   <Portfolio>
     <div
       class="hero is-medium is-primary"
-      :style="{
-        background:
-          'linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.9)), url(' +
-          $page.strapiPortfolio.cover.url +
-          ')',
-        'background-attachment': 'fixed',
-        'background-position': 'center',
-        'background-repeat': 'no-repeat',
-        'background-size': 'cover',
-      }"
+      :style="coverBackground($page.strapiPortfolio.cover.url)"
     >
       <div class="hero-head">
         <Navbar :color-mode="true" />
       </div>
       <div class="hero-body">
-        <p class="is-size-7 has-text-centered uppercase mb-2">
+        <p
+          class="is-size-7 has-text-centered is-uppercase mb-2 has-text-weight-light"
+        >
           {{ $page.strapiPortfolio.category.categoryName }}
         </p>
         <h1 class="title is-1 has-text-centered is-size-3-mobile">
@@ -65,19 +58,40 @@
           :key="image.id"
           class="tile"
           :class="{
-            'is-6': $page.strapiPortfolio.images.length % 2 === 0,
-            'is-4': $page.strapiPortfolio.images.length % 3 === 0,
+            'is-12': $page.strapiPortfolio.images.length === 1,
+            'is-6':
+              $page.strapiPortfolio.images.length % 2 === 0 &&
+              $page.strapiPortfolio.images.length % 3 !== 0,
+            'is-4':
+              $page.strapiPortfolio.images.length % 3 === 0 &&
+              $page.strapiPortfolio.images.length > 4,
           }"
           v-for="image in $page.strapiPortfolio.images"
         >
-          <div class="container has-text-centered">
-            <g-image :src="image.url" />
+          <div class="container has-text-centered on-hover">
+            <g-link :href="image.url" target="_blank"
+              ><g-image :src="image.url"
+            /></g-link>
           </div>
         </div>
       </div>
     </template>
   </Portfolio>
 </template>
+
+<style lang="scss" scoped>
+@for $i from 1 through 20 {
+  .tile:nth-child(#{$i}) {
+    overflow: hidden;
+    .on-hover {
+      transition: 0.2s transform;
+    }
+    .on-hover:hover {
+      transform: scale(1.05);
+    }
+  }
+}
+</style>
 
 <script>
 import Navbar from "@/components/TheNavbar.vue";
@@ -93,15 +107,20 @@ export default {
   mounted() {
     this.$aos.init();
   },
+  methods: {
+    coverBackground(url) {
+      return {
+        background:
+          "linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.9)), url(" + url + ")",
+        "background-attachment": "fixed",
+        "background-position": "center",
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+      };
+    },
+  },
 };
 </script>
-
-<style scope lang="scss">
-.uppercase {
-  text-transform: uppercase;
-  font-weight: 300;
-}
-</style>
 
 <page-query>
 query($id: ID!){
