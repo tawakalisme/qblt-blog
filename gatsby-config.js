01dc3f6925a50492167750b27e335b7dd4e4f3b2
Muhamad Iqbal Tawakal <tawakalisme@gmail.com>
2021-07-12 20:55:38 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Iqbal Tawakal",
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"],
+};
